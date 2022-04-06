9d8aa5ac785989f3a8b9cb03079e7b160f82524f
HXLMY <helmywahyudi0@gmail.com>
2022-04-06 11:53:25 +0800

    terbaik insyaallah

--- forms/neuweb.php
+++ forms/neuweb.php
@@ -9,6 +9,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="../assets/css/style.css" />
+    k
   </head>
   <body>
     <div class="container">
